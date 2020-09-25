0071efc171688564c619ef4ffd1ccb94ba9245da
tonguwin <ttn135@ucsd.edu>
2020-09-24 22:18:21 -0700

    test 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: "gatsby-source-prismic-graphql",
       options: {
-        repositoryName: "todnguyen.prismic.io", // (REQUIRED, replace with your own)
+        repositoryName: "todnguyen", // (REQUIRED, replace with your own)
         linkResolver: () => post => `/${post.uid}`,
       },
     },
