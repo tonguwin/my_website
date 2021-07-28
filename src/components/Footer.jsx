22a3494efc67e8a14e615cef46bd6b9c21aa9e0f
tonguwin <ttn135@ucsd.edu>
2021-07-27 17:27:24 -0700

    changed title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -62,7 +62,7 @@ const Footer = () => (
       <Logo />
     </Link>
     <FooterAuthor  className='zoom' href="https://github.com/tonguwin">
-      Website last updated through Github on 6.13.2021
+      Website last updated through Github on 7.27.2021
     </FooterAuthor>
   </FooterContainer>
 )
